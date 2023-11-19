a18ff01c92d638e92284382e02961107815a5e8b
yusshu <yushu@unnamed.team>
2023-11-19 02:13:38 -0500

    build: set up published pom details and signing

--- buildSrc/src/main/kotlin/creative.publishing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/creative.publishing-conventions.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     id("creative.java-conventions")
     `maven-publish`
+    signing
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 val repositoryName: String by project
@@ -19,7 +25,35 @@ publishing {
     }
     publications {
         create<MavenPublication>("maven") {
-            from(getComponents().getByName("java"))
+            from(components["java"])
+            pom {
+                name.set("${project.group}:${project.name}")
+                description.set(project.description)
+                url.set("https://github.com/unnamed/creative-central")
+                packaging = "jar"
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/license/mit/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("yusshu")
+                        name.set("Andre Roldan")
+                        email.set("yusshu@unnamed.team")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/unnamed/creative-central.git")
+                    developerConnection.set("scm:git:ssh://github.com:unnamed/creative-central.git")
+                    url.set("https://github.com/unnamed/creative-central")
+                }
+            }
         }
     }
+}
+
+signing {
+    sign(publishing.publications["maven"])
 }
